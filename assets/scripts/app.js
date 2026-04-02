2c9339afcad0b7ec23c725443ae2d3febec498d8
vinitjha <vinitjha.aec@gmail.com>
2026-04-02 00:15:13 -0700

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,14 @@ let currentResult = defaultResult;
 
 function add(num1, num2){
     const result =  num1 + num2;
-    alert('The result is'+ result)
+    return result;
+    
+
 }
 add(1,2);
 add(5,5);
-currentResult = (currentResult + 10) * 3/2-1;
-let calculationDescription = `(${defaultResult} + '10)*3/2-1`;
+//currentResult = (currentResult + 10) * 3/2-1;
+//let calculationDescription = `(${defaultResult} + '10)*3/2-1`;
 let errorMessage = 'An error \n' + 'Occured!';
+currentResult = add(1,2)
 outputResult(currentResult, errorMessage);
