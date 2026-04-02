20d7fe6b523a659c70943bf955b184ff03aa1578
vinitjha <vinitjha.aec@gmail.com>
2026-04-01 22:06:52 -0700

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-currentResult = (currentResult + 10) * 3/2-1;
+defaultResult = (currentResult + 10) * 3/2-1;
 let calculationDescription = '(' + currentResult + '10)*3/2-1';
 outputResult(currentResult, calculationDescription);
