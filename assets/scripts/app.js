12116c1dd1d6833541d4e0a521d02f774e22be4b
vinitjha <vinitjha.aec@gmail.com>
2026-04-07 04:50:58 -0700

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-    currentResult = int(currentResult + userInput.value)
+    currentResult = currentResult + parseInt(userInput.value)
     outputResult(currentResult, '')
     
 }
