19508ecb25584fe2b7cb9f54146d81dc8529f1d5
vinitjha <vinitjha.aec@gmail.com>
2026-04-07 04:00:45 -0700

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2){
-    const result =  num1 + num2;
-    return result;
+function add(){
+    currentResult = int(currentResult + userInput.value);
+    outputResult(currentResult,calculationDescription)
     
-
 }
-add(1,2);
-add(5,5);
-//currentResult = (currentResult + 10) * 3/2-1;
-//let calculationDescription = `(${defaultResult} + '10)*3/2-1`;
-let errorMessage = 'An error \n' + 'Occured!';
-currentResult = add(1,2)
-outputResult(currentResult, errorMessage);
+addBtn.addEventListener('click', add);
