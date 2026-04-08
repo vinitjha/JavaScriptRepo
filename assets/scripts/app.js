ad179ec258bf97ca57b29881d7615bb75c9182ab
vinitjha <vinitjha.aec@gmail.com>
2026-04-08 05:36:58 -0700

    updating changes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,38 +1,44 @@
-//calculator app
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function createAndWriteOutput(operator,resultBeforeCalc,calcNumber){
-    const calcDescription= `${resultBeforeCalc} ${operator} ${calcNumber}`}
-    outputResult(currentResult, calcDescription);
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
 
 function add() {
-    const enteredNumber = parseInt(userInput.value);
-    const initialResult = currentResult
-    currentResult = currentResult + parseInt(enteredNumber);
-    createAndWriteOutput('+', initialResult, enteredNumber)
-    }
-function substract() {
-    const enteredNumber = parseInt(userInput.value);
-    const initialResult = currentResult
-    currentResult = currentResult - parseInt(enteredNumber);
-    createAndWriteOutput('-', initialResult, enteredNumber)
-    }
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
 function multiply() {
-    const enteredNumber = parseInt(userInput.value);
-    const initialResult = currentResult
-    currentResult = currentResult * parseInt(enteredNumber);
-    createAndWriteOutput('*', initialResult, enteredNumber)
-    }
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
 function divide() {
-    const enteredNumber = parseInt(userInput.value);
-    const initialResult = currentResult
-    currentResult = currentResult * parseInt(enteredNumber);
-    createAndWriteOutput('/', initialResult, enteredNumber)
-    }    
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
 
 addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', substract);
+subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
-
