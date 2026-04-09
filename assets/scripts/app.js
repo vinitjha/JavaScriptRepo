862af5a7de74481b0066b2a60aa199e0d69ab8d4
vinitjha <vinitjha.aec@gmail.com>
2026-04-09 04:03:52 -0700

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,7 +17,7 @@ function add() {
   currentResult = currentResult + enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
   logEntries.push(enteredNumber);
-  console.log(logEntries);
+  console.log(logEntries[0]);
 }
 
 function subtract() {
